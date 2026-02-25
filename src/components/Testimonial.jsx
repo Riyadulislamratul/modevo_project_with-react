8b1b85ff40f375c8d9af6d14f657799c22c1a704
Riyadulislamratul <riadul1810833319@gmail.com>
2026-02-25 21:27:33 +0600

    Testimonial Section added

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import PortfolioCard from "./PortfolioCard";
 
-const Testimonial = () => {
-  return (
-    <div>Testimonial</div>
-  )
-}
+const TestimonialSection = () => {
+	return (
+    <section className="mt-25 mb-30">
+			<div className="container">
+				<div className="text-center mb-12">
+					<p className="mt-3 text-gray-500 max-w-2xl mx-auto">Portfolio</p>
+					<h2 className="text-3xl font-semibold text-gray-900">
+						What our customers say
+					</h2>
+				</div>
+				<div className="grid grid-cols-4 gap-6">
+					<PortfolioCard />
+					<PortfolioCard />
+					<PortfolioCard />
+					<PortfolioCard />
+				</div>
+        </div>
+        </section>
+	);
+};
 
-export default Testimonial
+export default TestimonialSection;
